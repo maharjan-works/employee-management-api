e8214bf0f12f7ac0238bff81aa3e6fba643f83ed
maharjan.works <maharjan.works@gmail.com>
2023-11-20 15:30:22 -0500

    interface EmployeeRepository.java created in repository package

---
+++ src/main/java/com/maharjanworks/employeemanagementapi/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.maharjanworks.employeemanagementapi.repository;
+
+import com.maharjanworks.employeemanagementapi.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
