28d136b69bb6192fb6713ccc0676d9ab5a449726
maharjan.works <maharjan.works@gmail.com>
2023-11-20 15:25:43 -0500

    class Employee.java is created in model package

---
+++ src/main/java/com/maharjanworks/employeemanagementapi/model/Employee.java
@@ -0,0 +1,70 @@
+package com.maharjanworks.employeemanagementapi.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employee_tbl")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long employeeId;
+    @Column(name="first_name")
+    private String firstName;
+    @Column(name="last_name")
+    private String lastName;
+    @Column(name="email_id")
+    private String emailId;
+
+    public Employee() {
+    }
+
+    public Employee(long employeeId, String firstName, String lastName, String emailId) {
+        this.employeeId = employeeId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailId = emailId;
+    }
+
+    public long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeId=" + employeeId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", emailId='" + emailId + '\'' +
+                '}';
+    }
+}
