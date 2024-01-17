27297b6394690e07481bc9bd2e0e775acd6023f9
maharjan.works <maharjan.works@gmail.com>
2024-01-17 14:03:11 -0500

    Lombok dependency added and entity updated.

--- src/main/java/com/maharjanworks/employeemanagementapi/model/Employee.java
+++ src/main/java/com/maharjanworks/employeemanagementapi/model/Employee.java
@@ -1,63 +1,27 @@
 package com.maharjanworks.employeemanagementapi.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "employee_tbl")
 public class Employee {
 
     @Id
-    @Column(name="employee_id")
+    @Column(name = "employee_id")
     private String employeeId;
-    @Column(name="first_name")
+    @Column(name = "first_name")
     private String firstName;
-    @Column(name="last_name")
+    @Column(name = "last_name")
     private String lastName;
-    @Column(name="email_id")
+    @Column(name = "email_id")
     private String emailId;
-
-    public Employee() {
-    }
-
-    public String getEmployeeId() {
-        return employeeId;
-    }
-
-    public void setEmployeeId(String employeeId) {
-        this.employeeId = employeeId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmailId() {
-        return emailId;
-    }
-
-    public void setEmailId(String emailId) {
-        this.emailId = emailId;
-    }
-
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "employeeId='" + employeeId + '\'' +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", emailId='" + emailId + '\'' +
-                '}';
-    }
 }
+
+
